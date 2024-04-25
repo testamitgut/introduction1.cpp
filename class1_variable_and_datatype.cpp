e3a1fe5e70430067c44b301e9c0c0f958fd74a72
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-26 01:29:53 +0530

    Add files via upload

---
+++ class1_variable_and_datatype.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //variable declare
+    //variable declare 4 bytes
+  int a,b=20,c=30,e=11;
+  a=10; //variable define
+  cout<<a+b+c;
+
+
+
+char n='a';
+cout<<n;
+
+
+float num=2.31;
+cout<<num;
+
+double d=3.121;
+cout<<d;
+
+bool b1=false;
+cout<<b1;
+
+
+}
