59d45525afa2ba00fce4a37d96ed7dc4a150a93d
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-26 00:26:11 +0530

    Add files via upload

---
+++ class1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello Amit"<<endl<<"Hello Amit"<<endl;
+     cout<<"Hello Amit"<<endl;
+
+
+
+    cout<<3+2<<endl;
+
+    cout<<"3+2"; 
+
+}
