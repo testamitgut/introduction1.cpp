61e8a69c57f706a172c936439fea336ba100ee3e
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-26 01:39:15 +0530

    Add files via upload

---
+++ class2_user_input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"Enter the value of the num1"<<endl;
+    cin>>num1;
+    cout<<"Enter the value of the num2"<<endl;
+    cin>>num2;
+    cout<<num1+num2<<endl;
+}
