b222ac61faac361216e37ac287dad0be3bc6808e
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-26 10:47:02 +0530

    Add files via upload

---
+++ class2_question2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the value of the a :";
+    cin>>a;
+    cout<<"Enter the value of the b :";
+    cin>>b;
+    if(a>b){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+}
