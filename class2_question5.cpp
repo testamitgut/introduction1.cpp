8bdecbb74f1c01d473de189ae4c51ef2ce04014f
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-26 11:04:24 +0530

    Add files via upload

---
+++ class2_question5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number ;
+    cout<<"Enter the value of the number ";
+    cin>>number;
+    if(number>0){
+        cout<<"Positive ";
+
+    }
+    else if(number==0){
+        cout<<"Number is zero";
+    }
+   else{
+    cout<<"Negative";
+   }
+    
+}
