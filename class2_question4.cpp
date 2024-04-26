fadbe3baa906e3df9df36f9f311903c0b6402e9a
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-26 10:56:20 +0530

    Add files via upload

---
+++ class2_question4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter the age of the Man :";
+    cin>>age;
+    if(age>=18){
+        cout<<"Adult";
+    }
+    else{
+        cout<<"Teenager";
+    }
+}
