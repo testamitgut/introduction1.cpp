62d59d979a4424c7ee96a091f4d676a6fbd4a579
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-26 10:40:11 +0530

    Add files via upload

---
+++ class2_question1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks;
+    cout<<"Enter the marks";
+    cin>>marks;
+    if(marks>33){
+        cout<<"Pass";
+    }
+    else{
+        cout<<"Fail";
+    }
+}
