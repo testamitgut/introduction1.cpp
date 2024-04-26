8afbd2f80b06a23758b0ba9222b7d348f90572ef
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-26 11:14:08 +0530

    Add files via upload

---
+++ class2_question6.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char c;
+    cout<<"Enter the value of the c";
+    cin>>c;
+    if(c=='a'){
+        cout<<"Vowel";
+    }
+    else if(c=='e'){
+        cout<<"Vowel";
+    }
+    else if(c=='i'){
+        cout<<"Vowel";
+    }
+    else if(c=='u'){
+        cout<<"Vowel";
+
+    }else if(c=='o'){
+        cout<<"Vowel";
+
+    }
+else{
+    cout<<"consonant";
+}
+}
