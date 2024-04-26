2e93b7c34c1b0b50540e1fd5cf276c8e479fd2b7
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-26 10:37:13 +0530

    Add files via upload

---
+++ class2_if_else.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int package;
+    cout<<"Enter the value of the package";
+    cin>>package;
+    if(package>10){
+        cout<<"Accepted";
+    }
+    else{
+        cout<<"rejected";
+    }
+}
