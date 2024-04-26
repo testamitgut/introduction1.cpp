83100b32f4be048b339c48016b8e1942b9e994fc
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-26 10:53:36 +0530

    Add files via upload

---
+++ class2_question3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int n;
+    // cout<<"Enter the value of the n :";
+    // cin>>n;
+    // if(n%2==0){
+    //     cout<<"Number is Even ";
+
+    // }
+    // else{
+    //     cout<<"odd";
+    // }
+
+
+
+int n;
+cout<<"Enter the value of the n :";
+cin>>n;
+if(n%2==1){
+    cout<<"odd";
+}
+else{
+    cout<<"Even";
+}
+
+
+}
