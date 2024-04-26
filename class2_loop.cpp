b8483b64772f7d4773efe6dac5bf9811253a64cc
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-04-26 11:52:10 +0530

    Add files via upload

---
+++ class2_loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value of the  n";
+    cin>>n;
+    // for( int i=1;i<=n;i++){
+    //     cout<<i*i<<endl;
+    // }
+
+for(int i=1;i<=n;i++){
+    cout<<"Hello"<<endl;
+}  
+       
+}
