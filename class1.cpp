f2869759bd7a88e467bbb24c8f3c6af89a6433cd
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-06-09 11:15:09 +0530

    Add files via upload

--- class1.cpp
+++ class1.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
 using namespace std;
+class student{
+    public:
+    string name;
+    int age,roll_number;
+    string grade;
+};
 int main(){
-    cout<<"Hello Amit"<<endl<<"Hello Amit"<<endl;
-     cout<<"Hello Amit"<<endl;
-
-
-
-    cout<<3+2<<endl;
-
-    cout<<"3+2"; 
-
+student s1;
+s1.name="Amit";
+s1.age=10;
+s1.roll_number=2100400;
+s1.grade="A+";
+ cout<<s1.name;
 }
