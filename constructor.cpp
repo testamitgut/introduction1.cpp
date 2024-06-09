f402625c1acd2e67cce675376188b92fc409ab2e
AMIT KUMAR <136466964+testamitgut@users.noreply.github.com>
2024-06-09 15:31:15 +0530

    Add files via upload

--- constructor.cpp
+++ constructor.cpp
@@ -20,6 +20,15 @@ class customer
         account_numbar=1234;
         balance=123;
     }
+    customer(string name,int account_number,int balance){
+        this->name=name;
+        this->account_numbar=account_numbar;
+        this->balance=balance;
+
+    }
+  inline customer(string a,int b,int c):name(a),account_numbar(b),balance(c){
+
+  }
     void display1(){
         cout<<name<<account_numbar<<balance<<endl;
     }
@@ -29,7 +38,8 @@ class customer
         }
 };
 int main(){
-    customer A1,A3;
+    customer A1,A3,A4;
+
     A3.display1();
 
     customer A2("Amit",2100,1000);
